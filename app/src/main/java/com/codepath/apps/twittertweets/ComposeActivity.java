dbfa32ed805afe8bf72cbd56a916722f0b74cf74
Terra Oldham <toldham@hearsaycorp.com>
2016-08-21 08:18:35 -0300

    Posting your Tweet setup

--- app/src/main/java/com/codepath/apps/twittertweets/ComposeActivity.java
+++ app/src/main/java/com/codepath/apps/twittertweets/ComposeActivity.java
@@ -3,8 +3,15 @@ package com.codepath.apps.twittertweets;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class ComposeActivity extends AppCompatActivity {
+
+    private Button onCompose;
+    private TextView tweetText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,6 +19,16 @@ public class ComposeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_compose);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        tweetText = (TextView) findViewById(R.id.tweet_text);
+        onCompose = (Button) findViewById(R.id.onCompose);
+        //onCompose.setOnClickListener((View.OnClickListener) this);
     }
+
+    public void onCompose(View v) {
+        // do something when the button is clicked
+        Toast.makeText(this, "Posting your Tweet", Toast.LENGTH_LONG).show();
+    }
+
 
 }
