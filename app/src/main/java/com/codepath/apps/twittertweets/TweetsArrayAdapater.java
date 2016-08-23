b25569093a0ac10291eb8fe35cc9c90958af89ac
Terra Oldham <toldham@hearsaycorp.com>
2016-08-22 23:07:36 -0400

    Opening screen and compose screen edits

--- app/src/main/java/com/codepath/apps/twittertweets/TweetsArrayAdapater.java
+++ app/src/main/java/com/codepath/apps/twittertweets/TweetsArrayAdapater.java
@@ -20,30 +20,6 @@ public class TweetsArrayAdapater extends ArrayAdapter<Tweet> {
         super(context, 0, tweets);
     }
 
-    /*
-    public class ParseRelativeDate {
-        // getRelativeTimeAgo("Mon Apr 01 21:16:23 +0000 2014");
-        public String getRelativeTimeAgo(String rawJsonDate) throws java.text.ParseException {
-            String twitterFormat = "EEE MMM dd HH:mm:ss ZZZZZ yyyy";
-            SimpleDateFormat sf = new SimpleDateFormat(twitterFormat, Locale.ENGLISH);
-            sf.setLenient(true);
-
-            String relativeDate = "";
-            try {
-                long dateMillis = sf.parse(rawJsonDate).getTime();
-                relativeDate = DateUtils.getRelativeTimeSpanString(dateMillis,
-                        System.currentTimeMillis(), DateUtils.SECOND_IN_MILLIS).toString();
-            } catch (ParseException e) {
-                e.printStackTrace();
-            }
-
-            return relativeDate;
-        }
-
-        //Twitter date format Fri Apr 09 12:53:54 +0000 2010
-    }
-    */
-
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         // Get Tweets
